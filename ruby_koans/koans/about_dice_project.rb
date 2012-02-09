d4cfce9a183e53613ee232b5368143f1313a3e0c
John Li <ivan.lishchinovskiy@gmail.com>
2012-02-09 17:14:41 +0200

    another bunch of passed test added

--- ruby_koans/koans/about_dice_project.rb
+++ ruby_koans/koans/about_dice_project.rb
@@ -6,6 +6,15 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def roll(number)
+    @values = Array.new
+    number.times { @values << rand(6)+1}
+  end
+end
+
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
